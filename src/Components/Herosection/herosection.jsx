c05b76f1abfcea27977bea58c6214812b0bfd0e0
CbnMarian <m.marian03@yahoo.com>
2024-01-25 01:33:40 +0200

    first commit

---
+++ src/Components/Herosection/herosection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./herosection.css";
+const Hero = () => {
+  return (
+    <section className="container">
+      <div className="container-bg">
+        <div className=""></div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
