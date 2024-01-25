39620b71c0df56e6ef0213516377884ee9433823
CbnMarian <m.marian03@yahoo.com>
2024-01-25 13:02:06 +0200

    added somthin new

--- src/Components/NavBar/nav.jsx
+++ src/Components/NavBar/nav.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import "./nav.scss";
 
 const Navigation = () => {
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="container-menu">
       <ul className="nav-ul">
-        <li className="nav-item">About</li>
-        <li className="nav-item">Price</li>
-        <li className="nav-item">Contact</li>
+        <li className="nav-item" onClick={() => scrollToSection("about")}>
+          About
+        </li>
+        <li className="nav-item" onClick={() => scrollToSection("price")}>
+          Price
+        </li>
+        <li className="nav-item" onClick={() => scrollToSection("Contact")}>
+          Contact
+        </li>
       </ul>
     </div>
   );
--- src/Components/banner/banner.jsx
+++ src/Components/banner/banner.jsx
@@ -6,21 +6,24 @@ import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
 
 const Banner = () => {
   return (
-    <section className="banner">
+    <section id="Contact" className="banner">
       <video className="background-video" autoPlay loop muted>
         <source src={smokeVideo} type="video/mp4" />
       </video>
       <div className="banner-text">
         <h1>Contacteazane apasand un buton!</h1>
         <div className="banner-icons">
-          <a href="tel:your-phone-number">
-            <PhoneInTalkIcon />
+          <a href="tel:your-phone-number" className="heartbeat">
+            <PhoneInTalkIcon sx={{ fontSize: 35 }} />
           </a>
-          <a href="https://wa.me/your-whatsapp-number">
-            <WhatsAppIcon />
+          <a href="https://wa.me/your-whatsapp-number" className="heartbeat">
+            <WhatsAppIcon sx={{ fontSize: 35 }} />
           </a>
-          <a href="https://www.facebook.com/your-facebook-profile">
-            <FacebookIcon />
+          <a
+            href="https://www.facebook.com/your-facebook-profile"
+            className="heartbeat"
+          >
+            <FacebookIcon sx={{ fontSize: 35 }} />
           </a>
         </div>
       </div>
