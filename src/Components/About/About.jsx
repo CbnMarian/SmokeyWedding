a525ed5263c9cd48031552fe0cd6f15613598fd2
CbnMarian <m.marian03@yahoo.com>
2024-01-25 11:39:16 +0200

    2

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about">
+      <div className="container-why-us">
+        <h3>De ce să alegi mașina noastră de facut fum?</h3>
+        <ul className="container-why-us_ul">
+          <li className="container-why-us_ul_li">
+            Efect dramatic și spectaculos pentru primul dans al mirilor.
+          </li>
+          <li className="container-why-us_ul_li">
+            Creație ușoară și non-toxică pentru o experiență plăcută.
+          </li>
+          <li className="container-why-us_ul_li">
+            Control total asupra intensității fumului pentru a se potrivi
+            perfect cu atmosfera evenimentului tău.
+          </li>
+          <li className="container-why-us_ul_li">
+            Personal specializat pentru instalare și asistență în timpul
+            evenimentului.
+          </li>
+        </ul>
+        <p>
+          Transformă-ți nunta într-un basm de poveste cu ajutorul mașinii
+          noastre de facut fum! Contactează-ne astăzi pentru a rezerva și aduce
+          o notă de magie la cel mai frumos dans al vieții tale!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/Components/Herosection/herosection.jsx
+++ src/Components/Herosection/herosection.jsx
@@ -16,30 +16,6 @@ const Hero = () => {
               Transformă ringul de dans într-o scenă de basm și surprinde-ți
               invitații cu un moment unic și spectaculos.
             </p>
-            <div className="container-why-us">
-              <h3>De ce să alegi mașina noastră de facut fum?</h3>
-              <ul className="container-why-us_ul">
-                <li className="container-why-us_ul_li">
-                  Efect dramatic și spectaculos pentru primul dans al mirilor.
-                </li>
-                <li className="container-why-us_ul_li">
-                  Creație ușoară și non-toxică pentru o experiență plăcută.
-                </li>
-                <li className="container-why-us_ul_li">
-                  Control total asupra intensității fumului pentru a se potrivi
-                  perfect cu atmosfera evenimentului tău.
-                </li>
-                <li className="container-why-us_ul_li">
-                  Personal specializat pentru instalare și asistență în timpul
-                  evenimentului.
-                </li>
-              </ul>
-              <p>
-                Transformă-ți nunta într-un basm de poveste cu ajutorul mașinii
-                noastre de facut fum! Contactează-ne astăzi pentru a rezerva și
-                aduce o notă de magie la cel mai frumos dans al vieții tale!
-              </p>
-            </div>
           </div>
         </div>
       </div>
