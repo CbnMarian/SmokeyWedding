ebe96b7e6740da68853678229aa677eee7dee2ef
CbnMarian <m.marian03@yahoo.com>
2024-01-25 02:41:01 +0200

    added navbar

--- src/Components/NavBar/nav.jsx
+++ src/Components/NavBar/nav.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./nav.scss";
 
 const Navigation = () => {
-  return <div>text</div>;
+  return (
+    <div className="container-menu">
+      <ul>
+        <li className="nav-item">About</li>
+        <li className="nav-item">Price</li>
+        <li className="nav-item">Contact</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
