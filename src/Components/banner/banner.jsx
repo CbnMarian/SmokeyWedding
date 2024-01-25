41ee10fe838653b96bb22737dbe3d528dc3a3cfc
CbnMarian <m.marian03@yahoo.com>
2024-01-25 12:31:11 +0200

    5

--- src/Components/banner/banner.jsx
+++ src/Components/banner/banner.jsx
@@ -1,5 +1,8 @@
 import "./banner.scss";
 import smokeVideo from "../assets/smoke-wedding.mp4";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
 
 const Banner = () => {
   return (
@@ -9,6 +12,17 @@ const Banner = () => {
       </video>
       <div className="banner-text">
         <h1>Contacteazane apasand un buton!</h1>
+        <div className="banner-icons">
+          <a href="tel:your-phone-number">
+            <PhoneInTalkIcon />
+          </a>
+          <a href="https://wa.me/your-whatsapp-number">
+            <WhatsAppIcon />
+          </a>
+          <a href="https://www.facebook.com/your-facebook-profile">
+            <FacebookIcon />
+          </a>
+        </div>
       </div>
     </section>
   );
