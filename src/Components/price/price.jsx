436649cc36b0674f64d730e6b4203f923fa26b12
CbnMarian <m.marian03@yahoo.com>
2024-01-28 15:50:04 +0200

    added some text

--- src/Components/price/price.jsx
+++ src/Components/price/price.jsx
@@ -6,8 +6,28 @@ const Price = () => {
     <section id="Price" className="container-price">
       <div className="container-price-box">
         <div className="container-price_text">
-          <div>text</div>
-          <div>preturi</div>
+          <div>Preturi si pachete</div>
+          <div>
+            <p>
+              Pachet 1 - 700 lei - Fum greu - Gheata carbonica pentru paharele
+              de șampaniei la primirea invitatilor - Mașina baloane săpun - 4
+              dispozitive de iluminat fumul greu Pachet 2 - 800 lei - Fum greu -
+              Artificii vulcani 4 buc Pachet 3 - 1000 lei - Fum greu - Gheata
+              carbonica pentru paharele de șampaniei la primirea invitatilor -
+              Mașina baloane săpun - 4 dispozitive de iluminat fumul greu -
+              Artificii vulcani 4 buc Pachet 4 - 1900 lei - Fum greu - Gheata
+              carbonica pentru paharele de șampaniei la primirea invitatilor -
+              Artificii vulcani 4 buc - Oglinda magica 4 ore Pachet 5 - 2000 lei
+              - Fum greu - Gheata carbonica pentru paharele de șampaniei la
+              primirea invitatilor - Artificii vulcani 4 buc - 360 video both 4
+              ore Pachet 6 - 3000 lei - Fum greu - Gheata carbonica pentru
+              paharele de șampaniei la primirea invitatilor - Artificii vulcani
+              4 buc - 4 dispozitive de iluminat fumul greu - Oglinda magica 4
+              ore - 360 video both 4 ore La pachetele 1, 2 și 3 putem adaugă
+              Baloane Explozive la prețul de +1000 lei La pachetele 4, 5 și 6
+              prețul baloanelor explozive este de 800 lei
+            </p>
+          </div>
         </div>
         <div className="container-price_map">harta zona</div>
       </div>
