d5f438c0daf9fcecbef71f9c17a0b19199b5a7fd
CbnMarian <m.marian03@yahoo.com>
2024-01-30 11:07:11 +0200

    text

--- src/Components/price/price.jsx
+++ src/Components/price/price.jsx
@@ -11,32 +11,44 @@ const Price = () => {
           <div>
             <ul>
               <li>
-                Pachet 1 - 700 lei - Fum greu - Gheata carbonica pentru paharele
-                de șampaniei la primirea invitatilor - Mașina baloane săpun - 4
-                dispozitive de iluminat fumul greu
+                <span className="span-text">
+                  Pachet 1 - 700 lei - Fum greu -{" "}
+                </span>
+                Gheata carbonica pentru paharele de șampaniei la primirea
+                invitatilor - Mașina baloane săpun - 4 dispozitive de iluminat
+                fumul greu
               </li>
               <li>Pachet 2 - 800 lei - Fum greu - Artificii vulcani 4 buc</li>
               <li>
-                Pachet 3 - 1000 lei - Fum greu - Gheata carbonica pentru
-                paharele de șampaniei la primirea invitatilor - Mașina baloane
-                săpun - 4 dispozitive de iluminat fumul greu - Artificii vulcani
-                4 buc
+                <span className="span-text">
+                  Pachet 3 - 1000 lei - Fum greu -
+                </span>{" "}
+                Gheata carbonica pentru paharele de șampaniei la primirea
+                invitatilor - Mașina baloane săpun - 4 dispozitive de iluminat
+                fumul greu - Artificii vulcani 4 buc
               </li>
               <li>
-                Pachet 4 - 1900 lei - Fum greu - Gheata carbonica pentru
-                paharele de șampaniei la primirea invitatilor - Artificii
-                vulcani 4 buc - Oglinda magica 4 ore
+                <span className="span-text">
+                  Pachet 4 - 1900 lei - Fum greu -
+                </span>{" "}
+                Gheata carbonica pentru paharele de șampaniei la primirea
+                invitatilor - Artificii vulcani 4 buc - Oglinda magica 4 ore
               </li>
               <li>
-                Pachet 5 - 2000 lei - Fum greu - Gheata carbonica pentru
-                paharele de șampaniei la primirea invitatilor - Artificii
-                vulcani 4 buc - 360 video both 4 ore
+                <span className="span-text">
+                  Pachet 5 - 2000 lei - Fum greu -
+                </span>{" "}
+                Gheata carbonica pentru paharele de șampaniei la primirea
+                invitatilor - Artificii vulcani 4 buc - 360 video both 4 ore
               </li>
               <li>
-                Pachet 6 - 3000 lei - Fum greu - Gheata carbonica pentru
-                paharele de șampaniei la primirea invitatilor - Artificii
-                vulcani 4 buc - 4 dispozitive de iluminat fumul greu - Oglinda
-                magica 4 ore - 360 video both 4 ore
+                <span className="span-text">
+                  Pachet 6 - 3000 lei - Fum greu -
+                </span>{" "}
+                Gheata carbonica pentru paharele de șampaniei la primirea
+                invitatilor - Artificii vulcani 4 buc - 4 dispozitive de
+                iluminat fumul greu - Oglinda magica 4 ore - 360 video both 4
+                ore
               </li>
               <li>
                 La pachetele 1, 2 și 3 putem adaugă Baloane Explozive la prețul
