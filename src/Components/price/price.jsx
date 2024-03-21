614ea787032f6b6e57f006d8d1b31f512fbf8304
CbnMarian <m.marian03@yahoo.com>
2024-03-21 14:20:07 +0200

    5

--- src/Components/price/price.jsx
+++ src/Components/price/price.jsx
@@ -26,13 +26,11 @@ const Price = () => {
                 <br />- Artificii vulcani 4 buc.
               </li>
               <li>
-                <span className="span-text">Pachet 4 - 1.000 lei.</span>
+                <span className="span-text">Pachet 4 - 900 lei.</span>
                 <br />
                 - Fum greu.
-                <br />- Gheata carbonica pentru paharele de șampaniei la
-                primirea invitatilor .
                 <br /> - 4 dispozitive de iluminat fumul greu.
-                <br /> - Artificii vulcani 4/8 buc.
+                <br /> - Artificii vulcani 4 buc.
               </li>
             </ul>
           </div>
